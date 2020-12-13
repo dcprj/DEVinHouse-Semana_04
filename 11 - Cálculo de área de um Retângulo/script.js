11a4e575cbc77b48b4dcd39680399d0f9c5e7fd7
Dario Pinheiro <dpinheiro@gmail.com>
2020-12-13 17:16:34 -0300

    [SP04] 11 - Calculo de area de um Retangulo

---
+++ 11 - Cálculo de área de um Retângulo/script.js
@@ -0,0 +1,8 @@
+const retangulo = {
+    lado1 : 10,
+    lado2 : 20
+}
+
+const areaRetangulo = (retangulo) => retangulo.lado1 * retangulo.lado2;
+
+console.log (`Área do retangulo com lado 1 = ${retangulo.lado1} e lado 2 = ${retangulo.lado2} é: ${areaRetangulo(retangulo)}`);
